bd3a0b944b414dafe37c195e3d4df10154c2b5cb
Morteza <75830370+mbaniadam@users.noreply.github.com>
2023-06-08 14:16:49 +0330

    Create init.sql

---
+++ postgres_docker/init.sql
@@ -0,0 +1,10 @@
+GRANT ALL PRIVILEGES ON DATABASE wgpeers TO postgres;
+\connect wgpeers
+CREATE TABLE users (
+        user_id serial PRIMARY KEY,
+        username VARCHAR (255) NOT NULL,
+        ip_address VARCHAR (255) NOT NULL,
+        created_date DATE NOT NULL DEFAULT CURRENT_DATE,
+		expire_date DATE NOT NULL
+
+);
