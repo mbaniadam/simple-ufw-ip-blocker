af97893bb138fe19a53306233d821648dba3408b
Morteza Baniadam <mortalbm@gmail.com>
2023-06-08 19:21:22 +0330

    added remove user section

--- add_user_in_db.py
+++ add_user_in_db.py
@@ -11,30 +11,62 @@ cnx = psycopg2.connect(database=db_name,
                         password="pgmor@1010",
                         port=db_port)
 cursor = cnx.cursor()
-finish = 0
 
-while finish == 0:
-    finish = 1
-    print("                         Add wg peers")
-    user = input("Enter User: ")
-    ip_address = input("Enter ip_address: ")
-    expire_date = input("Format: yyyy/mm/dd\nEnter expiration date: ")
-    """ insert a new user into the users table """
-    sql = f"""INSERT INTO {table_name} (username,ip_address,expire_date) VALUES ('{user}','{ip_address}','{expire_date}')
-            ON CONFLICT (ip_address) DO UPDATE SET expire_date = EXCLUDED.expire_date;"""
-    try:
-        cursor.execute(sql)
-        #print(cursor.execute(f"INSERT IGNORE INTO {table_name} ({user}, {ip_address}, {expire_date}) VALUES (%s,%s,%s)",
-        #    (user,ip_address,expire_date)))
-        end = input("would you like add another record? (y/n) ")
-        if end == "y":
-            finish = 0
-        else:
-            cnx.commit()
-            wg_expiration.main()
-            continue
-        
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
+def addUpUser():
+    finish = 0
+    while finish == 0:
+        finish = 1
+        print("                         Add wg peers")
+        user = input("Enter User: ")
+        ip_address = input("Enter ip_address: ")
+        expire_date = input("Format: yyyy/mm/dd\nEnter expiration date: ")
+        """ insert a new user into the users table """
+        sql = f"""INSERT INTO {table_name} (username,ip_address,expire_date) VALUES ('{user}','{ip_address}','{expire_date}')
+                ON CONFLICT (ip_address) DO UPDATE SET expire_date = EXCLUDED.expire_date;"""
+        try:
+            cursor.execute(sql)
+            #print(cursor.execute(f"INSERT IGNORE INTO {table_name} ({user}, {ip_address}, {expire_date}) VALUES (%s,%s,%s)",
+            #    (user,ip_address,expire_date)))
+            end = input("would you like add another record? (y/n) ")
+            if end == "y":
+                finish = 0
+            else:
+                cnx.commit()
+                wg_expiration.main()
+                continue
+            
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
 
-cnx.close()
+    cnx.close()
+
+
+def removeUser():
+    finish = 0
+    while finish == 0:
+        finish = 1
+        print("                         Remove wg peers")
+        ip_address = input("Enter ip_address: ")
+        sql = f"""DELETE FROM {table_name} WHERE ip_address = '{ip_address}';"""
+        try:
+            cursor.execute(sql)
+            end = input("would you like remove another record? (y/n) ")
+            if end == "y":
+                finish = 0
+            else:
+                cnx.commit()
+        except (Exception, psycopg2.DatabaseError) as error:
+            print(error)
+
+    cnx.close()
+
+
+
+
+X = input("Add >>> 1 - Remove >>> 2 : ")
+if X == "1":
+    addUpUser()
+elif X == "2":
+    removeUser()
+else:
+    print("wrong input!")
